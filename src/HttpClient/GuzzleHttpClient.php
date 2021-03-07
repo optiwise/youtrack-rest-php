26d35d3569920ea40897141bdfc734308dfde860
Manuel de Ruiter <manuel@inquiry.center>
2021-03-07 19:18:33 +0100

    Add support for Guzzle version 7.x

--- src/HttpClient/GuzzleHttpClient.php
+++ src/HttpClient/GuzzleHttpClient.php
@@ -86,7 +86,14 @@ class GuzzleHttpClient implements
      */
     private function appendUserAgent(array $options): array
     {
-        $defaultAgent = 'GuzzleHttp/' . Client::VERSION;
+
+        if (defined('GuzzleHttp\ClientInterface::MAJOR_VERSION')) {
+            $guzzleVersion = ClientInterface::MAJOR_VERSION;
+        } else {
+            $guzzleVersion = Client::VERSION;
+        }
+
+        $defaultAgent = 'GuzzleHttp/' . $guzzleVersion;
         if (extension_loaded('curl') && function_exists('curl_version')) {
             $curlVersion = \curl_version();
             if (\is_array($curlVersion)) {
